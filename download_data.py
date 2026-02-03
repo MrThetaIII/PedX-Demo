01800179e89515ada33937123fd15a0fbc982397
MrThetaIII <keep_mooving_forward@outlook.com>
2026-02-03 09:51:39 +0200

    Added calibration data

--- download_data.py
+++ download_data.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     basedir = 'data'
     mkdir(basedir)
 
-    capture_dates = ['20171130T2000', '20171207T2024', '20171212T2030']
-    for capture_date in capture_dates:
-        download_data(basedir, capture_date)
+    # capture_dates = ['20171130T2000', '20171207T2024', '20171212T2030']
+    # for capture_date in capture_dates:
+    #     download_data(basedir, capture_date)
     download_calib(basedir)
